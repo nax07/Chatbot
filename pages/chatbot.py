693e051a278b1852e44b5091d65d4cb36e52fa6f
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-11 14:30:43 +0200

    Update chatbot.py

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -6,13 +6,6 @@ def Chatbot_app():
     st.title("Chatbot")
     st.write("Puedes preguntar al Chatbot lo que necesites")
 
-    
-
-    # Store the initial value of widgets in session state
-    if "visibility" not in st.session_state:
-        st.session_state.visibility = "visible"
-        st.session_state.disabled = False
-
     col1, col2 = st.columns(2)
 
     with col1:
@@ -22,9 +15,7 @@ def Chatbot_app():
     with col2:
         option = st.selectbox(
             "Select LLM",
-            ("gpt2-medium", "banana phone", "19 $ fornite card"),
-            label_visibility=st.session_state.visibility,
-            disabled=st.session_state.disabled,
+            ("gpt2-medium", "banana phone", "19 $ fornite card")
         )
     
     with st.container():
