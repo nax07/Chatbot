38a2d967f8a62b7a82645c7919504387d1ffdfa7
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-12 00:19:27 +0200

    Update chatbot.py

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -36,8 +36,8 @@ def Chatbot_app():
     
     
     with st.container(border=True):
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
+        #if "messages" not in st.session_state:
+        #    st.session_state.messages = []
 
         for message in st.session_state.messages:
             with st.chat_message(message["role"]):
