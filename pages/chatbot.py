d42cea131dfa1547d4593e2b37d00bb935d6159f
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-11 15:59:22 +0200

    Update chatbot.py

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -28,15 +28,18 @@ def Chatbot_app():
 
     # Create space for the chatbot
     st.write('Chat')
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
     prompt = st.chat_input('¿Qué tal?')
+    
+    
+
     with st.container(border=True):
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+        
         if prompt:
             st.session_state.messages.append({"role": "user", "content": prompt})
             st.session_state.messages.append({"role": "assistant", "content": prompt})
