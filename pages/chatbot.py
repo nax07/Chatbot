44c19a233becf013520ae210ede971b7188a7726
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-11 14:51:40 +0200

    Update chatbot.py

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -28,17 +28,17 @@ def Chatbot_app():
                 st.write(f"Advanced Prompts: {'Enabled' if Adv_prompts else 'Disabled'}")
                 st.write(f"Selected LLM: {option}")
 
-            # Create space for the chatbot
-            st.write('Chat')
-            if "messages" not in st.session_state:
-                st.session_state.messages = []
-        
-            for message in st.session_state.messages:
-                with st.chat_message(message["role"]):
-                    st.markdown(message["content"])
+        # Create space for the chatbot
+        st.write('Chat')
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+    
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
 
-            prompt = st.chat_input('¿Qué tal?')
-            if prompt:
-                st.session_state.messages.append({"role": "user", "content": prompt})
-                st.session_state.messages.append({"role": "bot", "content": prompt})
-                st.experimental_rerun()
+        prompt = st.chat_input('¿Qué tal?')
+        if prompt:
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            st.session_state.messages.append({"role": "bot", "content": prompt})
+            st.experimental_rerun()
