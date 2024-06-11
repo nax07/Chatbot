a2270016e37d9a04c4b16f72a9ffba7221b44e61
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-11 11:14:08 +0200

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -1,10 +1,6 @@
 import streamlit as st
 
-from Info import Info_app
-from Chatbot import Chatbot_app
-from Chat_search import Search_app
-from QyA import QyA_app
-from LangStart import Lang_app
+from pages.chatbot import *
 
 from libraries.text_translation import *
 from libraries.text_processing import *
