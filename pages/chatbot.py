aad6592166bef1b024345387cc9db4fd0b0de29a
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-11 15:46:10 +0200

    Update chatbot.py

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -12,29 +12,31 @@ def Chatbot_app():
     with col1:
         RAG = st.checkbox("Enable RAG", key="enabled_RAG")
         Adv_prompts = st.checkbox("Enable advanced prompts", key="enabled_prompts")
+
+    with col2:
         option = st.selectbox(
             "Select LLM",
             ("gpt2-medium", "banana phone", "19 $ fornite card"),
             index=None,
             placeholder="Select LLM model..."
         )
-
+    
         # Button to confirm settings
         if st.button("Confirmar Configuraciones"):
             # Reset the chat history
             st.session_state.messages = []
 
-        # Create space for the chatbot
-        st.write('Chat')
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
-    
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+    # Create space for the chatbot
+    st.write('Chat')
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-        prompt = st.chat_input('¿Qué tal?')
-        if prompt:
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            st.session_state.messages.append({"role": "assistant", "content": prompt})
-            st.experimental_rerun()
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    prompt = st.chat_input('¿Qué tal?')
+    if prompt:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.session_state.messages.append({"role": "assistant", "content": prompt})
+        st.experimental_rerun()
