75772040e3997084ea2dd307d9d0d92c6f19540c
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-11 11:16:03 +0200

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 from pages.chatbot import *
+from pages.info import *
 
 #from libraries.text_translation import *
 #from libraries.text_processing import *
@@ -9,9 +10,10 @@ import streamlit as st
 
 PAGES = {
     "Info": Info_app,
-    "Chatbot": Chatbot_app,
-    "Chat with Search": Search_app,
+    "Chatbot": Chatbot_app
 }
+    #"Chat with Search": Search_app,
+
 
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
