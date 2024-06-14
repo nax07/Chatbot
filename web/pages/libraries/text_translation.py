d0553a8b0954031e04a2cdc19a0a055ce15ed0cc
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-14 18:26:46 +0200

    Update text_translation.py

--- web/pages/libraries/text_translation.py
+++ web/pages/libraries/text_translation.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 
 def translator(text, language1, language2):
-    modelo = f"Helsinki-NLP/opus-mt-{lan1}-{lan2}"
+    modelo = f"Helsinki-NLP/opus-mt-{language1}-{language2}"
     pipe = pipeline('translation', model=modelo)
     translated_text = pipe(text)[0]['translation_text']
     return translated_text
