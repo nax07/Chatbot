17a0a066a4478bfc6ce8b948e3be82ed9c06ae91
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-14 13:12:01 +0200

    Update 1 - Chatbot.py

--- web/pages/1 - Chatbot.py
+++ web/pages/1 - Chatbot.py
@@ -23,27 +23,7 @@ set_button = st.sidebar.button("Confirmar Configuraciones")
 if set_button:
     # Reset the chat history
     st.session_state.messages = []
-
-# Initial column for configuration options
-col1, col2 = st.columns(2)
-
-with col1:
-    RAG = st.checkbox("Enable RAG", key="enabled_RAG")
-    Adv_prompts = st.checkbox("Enable advanced prompts", key="enabled_prompts")
-
-with col2:
-    option = st.selectbox(
-        "Select LLM",
-        ("gpt2-medium", "banana phone", "19 $ fornite card"),
-    )
-
-# Button to confirm settings
-set_button =  st.button("Confirmar Configuraciones")
-if set_button:
-    # Reset the chat history
-    st.session_state.messages = []
         
-
 # Create space for the chatbot
 st.write('Chat')
 prompt = st.chat_input('¿Qué tal?')
