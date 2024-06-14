3b4f8af3256263665572d191f3a6b96dfd97f25a
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-14 11:38:18 +0200

    Update 2 - Buscador.py

--- web/pages/2 - Buscador.py
+++ web/pages/2 - Buscador.py
@@ -44,9 +44,10 @@ if selected_file:
                 if 'Images_URL' in data.columns:
                     st.markdown("**Imágenes:**")
                     st.write(data.loc[index, 'Images_URL'])
-                    for img_url in data.loc[index, 'Images_URL']:
+                    img_list = data.loc[index, 'Images_URL']
+                    for img_url in img_list:
                         st.write(f"{img_url}")
-                        st.image(img_url, width=200)
+                        st.image(img_url, width=200//len(img_list))
         else:
             column = st.selectbox("Seleccione la columna", data.columns)
             query = st.text_input(f"Ingrese el valor para buscar en la columna {column}")
