b95c47b7c97e5c200ad7ed22a3c0c7d663a6e52c
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-14 18:05:39 +0200

    Update text_translation.py

--- web/pages/libraries/text_translation.py
+++ web/pages/libraries/text_translation.py
@@ -60,10 +60,9 @@ def translator(text, language1, language2):
     lan1 = idioma_a_abreviacion.get(language1)
     lan2 = idioma_a_abreviacion.get(language2)
 
-    modelo = f"Helsinki-NLP/opus-mt-{language1}-{language2}"
-    return modelo
-    #pipe = pipeline('translation', model=modelo)
-    #translated_text = translator(text, max_length=5*len(text))[0]['translation_text']
-    #return translated_text
+    modelo = f"Helsinki-NLP/opus-mt-{lan1}-{lan2}"
+    pipe = pipeline('translation', model=modelo)
+    translated_text = translator(text, max_length=5*len(text))[0]['translation_text']
+    return translated_text
 
     
