d059b003596c1f90c17d91ea6a15b8461ba1355e
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-18 14:26:26 +0200

    Update text_processing.py

--- web/pages/libraries/text_processing.py
+++ web/pages/libraries/text_processing.py
@@ -3,7 +3,6 @@ from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 def model_loading(modelo):
     return pipeline("text-generation", model=modelo)
 
-
 def data_processing(text, pipe, RAG=False, Adv_prompts=False):
     prompt = f"Question: {text}.\nAnswer:"
     generated_text = pipe(prompt, max_length=100, num_return_sequences=1, truncation=True)
