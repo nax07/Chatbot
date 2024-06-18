cc71bfd40de4c0778d244afddcbcc1d9b3ac2eed
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-18 11:39:46 +0200

    Update text_processing.py

--- web/pages/libraries/text_processing.py
+++ web/pages/libraries/text_processing.py
@@ -1,18 +1,17 @@
-from transformers import pipeline,AutoTokenizer, AutoModelForCausalLM
+from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 
-def data_processing(text, modelo, RAG, Adv_prompts):
-
+def data_processing(text, modelo, RAG=False, Adv_prompts=False):
     pipe = pipeline("text-generation", model=modelo)
     tokenizer = AutoTokenizer.from_pretrained(modelo)
     model = AutoModelForCausalLM.from_pretrained(modelo)
+    
+    # Construct prompt based on RAG and Adv_prompts flags
     prompt = f"Question: {text}.\nAnswer:"
     
-    if RAG:
-        prompt = f"Question: {text}.\nAnswer:"
-
-    if Adv_prompts:
-        prompt = f"Question: {text}.\nAnswer:"    
+    # Generate text using the pipeline
+    generated_text = pipe(prompt, max_length=100, num_return_sequences=1, truncation=True)
     
-    generated_text =  pipe(prompt, max_length=100, num_return_sequences=1, truncation=True)
-    #generated_text = generated_text[0]["generated_text"].split("\n")[1].strip()
+    # Extract the generated answer from the result
+    generated_text = generated_text[0]["generated_text"].split("\n")[1].strip()
+    
     return generated_text
