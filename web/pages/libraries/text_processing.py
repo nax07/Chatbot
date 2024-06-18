d20e4bd4507ddb62b612bf9df45b817ab4e96449
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-18 13:55:42 +0200

    Update text_processing.py

--- web/pages/libraries/text_processing.py
+++ web/pages/libraries/text_processing.py
@@ -6,5 +6,5 @@ def model_loading(modelo):
 
 def data_processing(text, pipe, RAG=False, Adv_prompts=False):
     prompt = f"Question: {text}.\nAnswer:"
-    generated_text = pipe(prompt, max_length=100, num_return_sequences=1, truncation=True)t
+    generated_text = pipe(prompt, max_length=100, num_return_sequences=1, truncation=True)
     return generated_text[0]["generated_text"].split("\n")[1].strip()
