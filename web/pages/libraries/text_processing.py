5a9b3db1d3a3d3eeaaa188d01559a7ba40d213d2
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-18 11:33:05 +0200

    Update text_processing.py

--- web/pages/libraries/text_processing.py
+++ web/pages/libraries/text_processing.py
@@ -3,19 +3,16 @@ from transformers import pipeline,AutoTokenizer, AutoModelForCausalLM
 def data_processing(text, modelo, RAG, Adv_prompts):
 
     pipe = pipeline("text-generation", model=modelo)
-    return pipe
-    #tokenizer = AutoTokenizer.from_pretrained(modelo)
-    #model = AutoModelForCausalLM.from_pretrained(modelo)
-    #prompt = f"Question: {text}.\nAnswer:"
+    tokenizer = AutoTokenizer.from_pretrained(modelo)
+    model = AutoModelForCausalLM.from_pretrained(modelo)
+    prompt = f"Question: {text}.\nAnswer:"
     
-    #if RAG:
-    #    prompt = f"Question: {text}.\nAnswer:"
+    if RAG:
+        prompt = f"Question: {text}.\nAnswer:"
 
-    #if Adv_prompts:
-    #    prompt = f"Question: {text}.\nAnswer:"    
+    if Adv_prompts:
+        prompt = f"Question: {text}.\nAnswer:"    
     
-    #generated_text =  pipe(prompt, max_length=100, num_return_sequences=1, truncation=True)
+    generated_text =  pipe(prompt, max_length=100, num_return_sequences=1, truncation=True)
     #generated_text = generated_text[0]["generated_text"].split("\n")[1].strip()
-
-
-    #return generated_text
+    return generated_text
