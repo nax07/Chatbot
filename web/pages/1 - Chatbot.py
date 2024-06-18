d3551e08e2bda9742aff665ec56cb5c500df394b
nax07 <133920749+nax07@users.noreply.github.com>
2024-06-18 14:08:59 +0200

    Update 1 - Chatbot.py

--- web/pages/1 - Chatbot.py
+++ web/pages/1 - Chatbot.py
@@ -53,16 +53,28 @@ mod_selec = st.sidebar.selectbox(
 
 # Botón para confirmar configuraciones
 set_button = st.sidebar.button("Confirmar Configuraciones / Limpiar historial")
+
+if "lan_en" not in st.session_state:
+        st.session_state.lan_en = False
+    
+if "en_lan" not in st.session_state:
+        st.session_state.en_lan = False
+
+if "process" not in st.session_state:
+        st.session_state.process = False
+
 if set_button:
     # Reset the chat history
     st.session_state.messages = []
+
+    # Set selected configurations
     modelo = modelos.get(mod_selec)
-    process_pipe = model_loading(modelo)
+    st.session_state.process = model_loading(modelo)
     if idioma != "Inglés":
         lan1 = idioma_a_abreviacion.get(idioma)
         lan2 = "en"
-        lan_en_pipe = load_translator(lan1, lan2)
-        en_lan_pipe = load_translator(lan2, lan1)
+        st.session_state.lan_en = load_translator(lan1, lan2)
+        st.session_state.en_lan = load_translator(lan2, lan1)
         
 # Create space for the chatbot
 prompt = st.chat_input('Envía un mensaje')
@@ -75,14 +87,23 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 if prompt:
-    if idioma != "Inglés":
-        translated_prompt = translator(prompt, lan_en_pipe)
-        solution = data_processing(translated_prompt, process_pipe, RAG, Adv_prompts)
-        response = translator(solution, en_lan_pipe)
+    if st.session_state.process:
+        if idioma:   
+            if idioma != "Inglés":
+                translated_prompt = translator(prompt, st.session_state.lan_en)
+                solution = data_processing(translated_prompt, st.session_state.process, RAG, Adv_prompts)
+                response = translator(solution, st.session_state.en_lan)
+            else:
+                response = data_processing(prompt, st.session_state.process, RAG, Adv_prompts)
+            
+            
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            st.rerun()
+        else:
+            st.warning("Idioma no especificado. Por favor, seleccione un idioma y confirme las configuraciones.")
     else:
-        response = data_processing(prompt, process_pipe, RAG, Adv_prompts)
-    
-    
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.session_state.messages.append({"role": "assistant", "content": response})
-    st.rerun()
+        st.warning("El modelo no está cargado. Por favor, seleccione el modelo y confirme las configuraciones.")
+else:
+    st.warning("No se proporcionó ningún mensaje. Por favor, ingrese un mensaje.")
+        
