cc720cda40e1a07ff578e14a0e3b4a462e3c361d
nax07 <133920749+nax07@users.noreply.github.com>
2024-08-23 12:24:38 +0200

    Update 3 - Test.py

--- web/pages/3 - Test.py
+++ web/pages/3 - Test.py
@@ -47,7 +47,7 @@ idioma_a_abreviacion = {
 modelos = {
     "gpt2": "openai-community/gpt2",
     "Qwen-VL": "Qwen/Qwen-VL-Chat",
-    "dolly-v2-12b": "databricks/dolly-v2-12b"
+    "dolly-v2-12b": "databricks/dbrx-base"
 }
 
 
