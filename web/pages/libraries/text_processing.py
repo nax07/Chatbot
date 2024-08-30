586d2987f020a27921383391a26bbe769940268f
nax07 <133920749+nax07@users.noreply.github.com>
2024-08-30 17:14:13 +0200

    Update text_processing.py

--- web/pages/libraries/text_processing.py
+++ web/pages/libraries/text_processing.py
@@ -4,6 +4,9 @@ def model_loading(modelo):
     return pipeline("text-generation", model=modelo, trust_remote_code=True, return_full_text=True)
 
 def data_processing(text, pipe, RAG=False, Adv_prompts=False, max_len=100):
-    prompt = f"Question: {text}.\nAnswer:"
+    if Adv_prompts:
+        prompt = f"Question: {text}.\nAnswer:"
+    else:
+        prompt = f"Question: {text}.\nAnswer:"
     generated_text = pipe(prompt, num_return_sequences=1, truncation=True)
     return generated_text[0]["generated_text"].split("\n")[1].strip()
