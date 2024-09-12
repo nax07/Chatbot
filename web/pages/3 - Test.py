742912cb34076ea6eb984610ca89bf5a37a9291c
nax07 <133920749+nax07@users.noreply.github.com>
2024-09-12 14:08:23 +0200

    Update 3 - Test.py

--- web/pages/3 - Test.py
+++ web/pages/3 - Test.py
@@ -53,9 +53,9 @@ idioma = st.sidebar.selectbox(
 st.sidebar.subheader('Configuraciones del Chat')
 
 # Opción para activar/desactivar RAG
-RAG = st.sidebar.checkbox("Activar RAG", key="enabled_RAG")
+RAG = st.sidebar.checkbox("Activar RAG")
 if RAG:
-    files = st.sidebar.file_uploader("Sube los archivos de texto para hacer RAG aquí: ", accept_multiple_files=True)
+    RAG_files = st.sidebar.file_uploader("Sube los archivos de texto para hacer RAG aquí: ", accept_multiple_files=True)
 
 # Opción para activar/desactivar prompts avanzados
 Adv_prompts = st.sidebar.checkbox("Activar prompts avanzadas", key="enabled_prompts")
@@ -84,9 +84,11 @@ if set_button:
         st.session_state.process = model_loading(modelo)
     if idioma != "Inglés":
         lan1 = idioma_a_abreviacion.get(idioma)
-        lan2 = "en"
-        st.session_state.lan_en = load_translator(lan1, lan2)
-        st.session_state.en_lan = load_translator(lan2, lan1)
+        st.session_state.lan_en = load_translator(lan1, "en")
+        st.session_state.en_lan = load_translator("en", lan1)
+    if RAG:
+        st.sidebar.write(f"{RAG_files}")
+        
 
 
 # Create space for the chatbot
