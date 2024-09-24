c1d66c2092af2999a91e94b73f0169add4a096d4
nax07 <133920749+nax07@users.noreply.github.com>
2024-09-24 09:55:06 +0200

    Update text_processing.py

--- web/pages/libraries/text_processing.py
+++ web/pages/libraries/text_processing.py
@@ -1,12 +1,62 @@
-from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
+from langchain import hub
+from transformers import AutoTokenizer, pipeline
+from langchain import HuggingFacePipeline
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from transformers import AutoModelForCausalLM
+from langchain_huggingface.llms import HuggingFacePipeline
+from langchain_core.prompts import ChatPromptTemplate
 
-def model_loading(modelo):
-    return pipeline("text-generation", model=modelo, trust_remote_code=True, return_full_text=True)
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
-def data_processing(text, pipe, RAG=False, Adv_prompts=False, max_len=100):
+def llm_loading(model_name):
+
+    hf = HuggingFacePipeline.from_model_id(
+        model_id=model_name,
+        task="text-generation",
+        model_kwargs={"temperature": 0.7, "trust_remote_code": True},
+        pipeline_kwargs={"max_new_tokens": 100},
+    )
+
+    return hf
+
+def data_processing(question, Adv_prompts, RAG, llm, embeddings):
     if Adv_prompts:
-        prompt = f"Question: {text}.\nAnswer:"
+        template = """
+        You are a question-answering assistant. Answer the question. If you don’t know the answer, simply say you don’t know. Use concise sentences, no more than 3.
+
+        Question: {question}
+
+        Answer:
+        """
+
+        prompt = ChatPromptTemplate.from_template(template)
+        adv_chain = (
+            RunnableParallel({"question": RunnablePassthrough()})
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
+
+        output = adv_chain.invoke(question)
+        return output.split("Answer:")[1].strip()
+
+    elif RAG:
+        vectorstore = FAISS.load_local(vectorstore_path, embeddings, allow_dangerous_deserialization=True)
+        retriever = vectorstore.as_retriever(search_kwargs={"k": 10})
+        prompt = hub.pull("rlm/rag-prompt")
+        llm = llm_loading(model_name)
+        rag_chain = (
+            RunnableParallel({"context": retriever | format_docs, "question": RunnablePassthrough()})
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
+        return rag_chain.invoke(question)
     else:
-        prompt = f"Question: {text}.\nAnswer:"
-    generated_text = pipe(prompt, num_return_sequences=1, truncation=True)
-    return generated_text[0]["generated_text"].split("\n")[1].strip()
+        return llm.invoke(question)
