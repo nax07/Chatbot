f374f16e3a7d2b96c77ca55b691c432e281124bf
nax07 <133920749+nax07@users.noreply.github.com>
2024-09-29 13:37:26 +0200

    Update 2 - Testbot.py

--- web/pages/2 - Testbot.py
+++ web/pages/2 - Testbot.py
@@ -3,24 +3,20 @@ import streamlit as st
 import os
 import sys
 from langchain import hub
-
-# Add path for the imports
-sys.path.append('/mount/src/chatbot/web/pages/libraries')
-
-from text_processing import *
-from text_translation import *
-
-# Inicialize session state
-st.session_state.setdefault("idioma", "Inglés")
-st.session_state.setdefault("modelo", "gpt2-medium")
-st.session_state.setdefault("lan_en", False)
-st.session_state.setdefault("en_lan", False)
-st.session_state.setdefault("process", False)
-st.session_state.setdefault("embeddings", False)
-st.session_state.setdefault("vectorstore", False)
-st.session_state.setdefault("messages", [])
+from transformers import AutoTokenizer, pipeline
+from langchain import HuggingFacePipeline
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from transformers import AutoModelForCausalLM
+from langchain_huggingface.llms import HuggingFacePipeline
+from langchain_core.prompts import ChatPromptTemplate
 
 # Variables
+vectorstore_path = "/mount/src/chatbot/web/pages/vectorstore"
 idioma_a_abreviacion = {
     "Inglés": "en",
     "Español": "es",
@@ -39,7 +35,68 @@ modelos = {
     "modelo3": "openai-community/gpt2-medium"
 }
 
-vectorstore_path = "/mount/src/chatbot/web/pages/vectorstore"
+# Inicialize session state
+st.session_state.setdefault("idioma", "Inglés")
+st.session_state.setdefault("modelo", "gpt2-medium")
+st.session_state.setdefault("lan_en", False)
+st.session_state.setdefault("en_lan", False)
+st.session_state.setdefault("process", False)
+st.session_state.setdefault("embeddings", False)
+st.session_state.setdefault("vectorstore", False)
+st.session_state.setdefault("messages", [])
+
+# Auxiliar functions
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+def llm_loading(model_name):
+
+    hf = HuggingFacePipeline.from_model_id(
+        model_id=model_name,
+        task="text-generation",
+        model_kwargs={"temperature": 0.7, "trust_remote_code": True},
+        pipeline_kwargs={"max_new_tokens": 100},
+    )
+
+    return hf
+
+def data_processing(question, Adv_prompts, RAG, llm, embeddings, vectorstore):
+    if Adv_prompts:
+        template = """
+        You are a question-answering assistant. Answer the question. If you don’t know the answer, simply say you don’t know. Use concise sentences, no more than 3.
+
+        Question: {question}
+
+        Answer:
+        """
+
+        prompt = ChatPromptTemplate.from_template(template)
+        adv_chain = (
+            RunnableParallel({"question": RunnablePassthrough()})
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
+
+        output = adv_chain.invoke(question)
+        return output.split("Answer:")[1].strip()
+
+    elif RAG:
+        vectorstore = FAISS.load_local(vectorstore_path, embeddings, allow_dangerous_deserialization=True)
+        retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
+        prompt = hub.pull("rlm/rag-prompt")
+        rag_chain = (
+            RunnableParallel({"context": retriever | format_docs, "question": RunnablePassthrough()})
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
+        output = rag_chain.invoke(question)
+        return output.split("Answer:")[1].strip()
+    else:
+        return llm.invoke(question)
+
+
 
 ## Main App
 st.title("Chatbot_Test")
